04cf291197df9736308be636ada48f3db9bcc469
fusion809 <brentonhorne77@gmail.com>
2016-05-25 19:48:06 +1000

    Removing unneeded config.cson lines and packages.cson

--- quick-install.sh
+++ quick-install.sh
@@ -16,9 +16,14 @@ if ! `which apm >/dev/null 2>&1`; then
   cd -
 fi
 
-cd ~/.atom
-git init
+cd $HOME/.atom
+if ! [[ -d .git ]]; then
+  git init
+fi
+git remote rm origin
+git remote rm upstream
 git remote add origin git@github.com:fusion809/atom.git
+git remote add upstream git@github.com:fusion809/atom.git
 git pull origin master
 git submodule update --init --recursive
 cd -
