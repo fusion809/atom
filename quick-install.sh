f9396236bcdddeedc1547c6ec071901019aff441
fusion809 <brentonhorne77@gmail.com>
2016-06-01 07:55:35 +1000

    Updating

--- quick-install.sh
+++ quick-install.sh
@@ -2,7 +2,9 @@
 L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' | sed "s/\n/ /g")
 for i in "${L[@]}"
 do
-  apm install $i --no-confirm
+  if ! [[ -d $HOME/.atom/packages/$i ]]; then
+    apm install $i --no-confirm
+  fi
 done
 
 apm install https://github.com/fusion809/language-patch2 \
