a05dbc09c8cea4e37769527afa91518b216c444f
fusion809 <brentonhorne77@gmail.com>
2016-06-01 08:00:55 +1000

    Updating

--- quick-install.sh
+++ quick-install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' | sed "s/\n/ /g")
-for i in "${L[@]}"
+L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' )
+for i in "${L[$@]}"
 do
-  if ! [[ -d $HOME/.atom/packages/$i ]]; then
-    apm install $i --no-confirm
-  fi
+  apm install $i --no-confirm
 done
 
-apm install https://github.com/fusion809/language-patch2 \
-  https://github.com/fusion809/language-ini2 \
-  https://github.com/fusion809/browser-plus-fix
+apm install fusion809/language-patch2 fusion809/language-ini2
