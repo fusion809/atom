196184864ca05ecafa0ade6917d84319307fabd3
fusion809 <brentonhorne77@gmail.com>
2016-06-07 13:22:36 +1000

    Updating

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Atom
+
 This repository contains my own personal Atom settings. This includes my `config.cson`, `keymap.cson`, `packages.cson` and `projects.cson` files.
 
 ```bash
--- quick-install.sh
+++ quick-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' )
+
+apm install package-sync
+
 for i in "${L[$@]}"
 do
   apm install $i --no-confirm
