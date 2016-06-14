31516fd6805dbbb4377452905d14317ca6e5c55a
fusion809 <brentonhorne77@gmail.com>
2016-06-15 01:44:52 +1000

    Updating

--- quick-install.sh
+++ quick-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if ! `which atom > /dev/null 2>&1`; then
+  yaourt -S --noconfirm atom-editor-arch
+fi
+
 L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' )
 
 apm install package-sync
