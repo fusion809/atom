d17c487186e2fdd36fafd7aab19b96fdba2b6d03
BH2017 <fusion809@users.noreply.github.com>
2017-10-02 21:32:19 +1000

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! `which atom > /dev/null 2>&1`; then
-  yaourt -S --noconfirm atom-editor-arch
+  printf "Atom is not installed!"
 fi
 
 L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' )
