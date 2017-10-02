c9d35fe2a7d298333b66828464c504de4b3d081a
fusion809 <fusion809@users.noreply.github.com>
2017-10-02 21:39:44 +1000

    Fixing quick install

--- quick-install.sh
+++ quick-install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 if ! `which atom > /dev/null 2>&1`; then
-  printf "Atom is not installed!"
+    printf "Atom is not installed!"
+fi
+
+if ! [[ -d $HOME/.atom/.git ]]; then
+    if [[ -d $HOME/.atom ]]; then
+         mv $HOME/.atom $HOME/.atom.backup
+    fi
+    git clone https://github.com/fusion809/atom $HOME/.atom
+fi
+
+if ! [[ $PWD == "$HOME/.atom" ]]; then
+    cd $HOME/.atom
 fi
 
 L=$(cat packages.cson | sed 's/"//g' | sed 's/  //g' | sed 's/packages: \[//g' | sed 's/\]//g' )
@@ -9,5 +20,5 @@ apm install package-sync
 
 for i in "${L[$@]}"
 do
-  apm install $i --no-confirm
+    apm install $i --no-confirm
 done
